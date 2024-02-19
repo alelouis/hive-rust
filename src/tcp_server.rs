e91c52b7b6581f728bb6a0750b3c2b6abd573639
alelouis <me@alelouis.eu>
2024-02-19 22:39:15 +0100

    Interface with godot tcp socket

---
+++ src/tcp_server.rs
@@ -0,0 +1,49 @@
+mod engine;
+mod logic;
+
+use crate::engine::Engine;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{thread, time};
+
+fn handle_client(mut stream: &mut TcpStream, mut engine: &mut Engine) {
+    let mut response = String::new();
+    let mut conn = BufReader::new(&mut stream);
+    let length = conn.read_line(&mut response).expect("unable to read");
+
+    println!("{:?}", response);
+    let server_response = &response[4..];
+    let engine_response = engine.process_command(server_response.to_string());
+    let server_response = match engine_response {
+        Ok(r) => {
+            format!("{r}\nok")
+        }
+        Err(e) => {
+            format!("err {e}\nok")
+        }
+    };
+
+    stream
+        .write(server_response.as_bytes())
+        .expect("Failed to write response to client");
+}
+
+fn main() {
+    let mut engine = Engine::new();
+
+    let listener = TcpListener::bind("127.0.0.1:8181").expect("Failed to bind to address");
+    println!("Server listening on 127.0.0.1:8181");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => loop {
+                let sleep_duration = time::Duration::from_millis(200);
+                thread::sleep(sleep_duration);
+                handle_client(&mut stream, &mut engine);
+            },
+            Err(e) => {
+                eprintln!("Failed to establish connection: {}", e);
+            }
+        }
+    }
+}
