e72ec0191ea113bc99b48bd63540a07ce41c091e
alelouis <me@alelouis.eu>
2024-02-21 20:41:07 +0100

    correct ant placement

--- src/logic/bugs/ant.rs
+++ src/logic/bugs/ant.rs
@@ -16,5 +16,23 @@ pub fn moves(tile: Tile, active_bugs: &HashMap<Tile, Vec<Bug>>) -> HashSet<Tile>
     let mut candidates =
         bfs_reach(tile, |t| successors(&t, active_bugs)).collect::<HashSet<Tile>>();
     candidates.remove(&tile);
-    candidates
+
+    let mut connected_moves = vec![];
+    for m in candidates.iter() {
+        let occupied_neighbors: Vec<Tile> = m
+            .neighbors()
+            .iter()
+            .filter(|t| active_bugs.get(t).is_some())
+            .cloned()
+            .collect();
+
+        if occupied_neighbors.len() == 1 {
+            if *occupied_neighbors.first().unwrap() != tile {
+                connected_moves.push(*m);
+            }
+        } else {
+            connected_moves.push(*m);
+        }
+    }
+    connected_moves.iter().cloned().collect()
 }
